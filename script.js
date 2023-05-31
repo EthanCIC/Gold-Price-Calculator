c261ecdc3efe29435bc2d38ffcec15a084bed45c
陳奕丞 <ethancic@chenyichengdeMacBook-Pro.local>
2023-05-31 20:18:09 +0800

    wip

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ document.getElementById('calculator').addEventListener('input', function(e) {
 
     // 檢查兩個 input 是否都有值
     if (goldPrice !== "" && exchangeRate !== "") {
-        var output1 = goldPrice * 32.1507;
-        var output2 = output1 * exchangeRate;
-        var output3 = output2 / 266.66;
+        var output1 = (goldPrice * 32.1507).toFixed(2);
+        var output2 = (output1 * exchangeRate).toFixed(2);
+        var output3 = (output2 / 266.66).toFixed(2);
 
-        document.getElementById('output1').innerText = output1.toFixed(2);
-        document.getElementById('output2').innerText = output2.toFixed(2);
-        document.getElementById('output3').innerText = output3.toFixed(2);
+        document.getElementById('output1').innerText =  numberWithCommas(output1);
+        document.getElementById('output2').innerText = numberWithCommas(output2);
+        document.getElementById('output3').innerText = numberWithCommas(output3);
     } else {
         // 其中一個或兩個 input 是空的，清空所有 output
         document.getElementById('output1').innerText = "";
@@ -18,3 +18,7 @@ document.getElementById('calculator').addEventListener('input', function(e) {
         document.getElementById('output3').innerText = "";
     }
 });
+
+function numberWithCommas(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
