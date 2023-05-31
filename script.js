bda6e666a1d21b007e02497f635cf446bc58c6cc
陳奕丞 <ethancic@chenyichengdeMacBook-Pro.local>
2023-05-31 18:36:03 +0800

    add html, css, js file

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('calculator').addEventListener('input', function(e) {
+    var goldPrice = document.getElementById('goldPrice').value;
+    var exchangeRate = document.getElementById('exchangeRate').value;
+
+    var output1 = goldPrice * 32.1507;
+    var output2 = output1 * exchangeRate;
+    var output3 = output2 / 266.66;
+
+    document.getElementById('output1').innerText = '一公斤價格（美元）：' + output1.toFixed(2) + ' (USD/kg)';
+    document.getElementById('output2').innerText = '一公斤價格（台幣）：' + output2.toFixed(2) + ' (TWD/kg)';
+    document.getElementById('output3').innerText = '一台斤價格（台幣）：' + output3.toFixed(2) + ' (TWD/台斤)';
+});
