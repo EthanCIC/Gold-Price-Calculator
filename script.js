0cdf55bf69faf57ddcc91cf6298100e3e9c3b0e1
陳奕丞 <ethancic@chenyichengdeMacBook-Pro.local>
2023-05-31 19:09:29 +0800

    wip

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('calculator').addEventListener('input', function(e) {
     var output2 = output1 * exchangeRate;
     var output3 = output2 / 266.66;
 
-    document.getElementById('output1').innerText = '一公斤價格（美元）：' + output1.toFixed(2) + ' (USD/kg)';
-    document.getElementById('output2').innerText = '一公斤價格（台幣）：' + output2.toFixed(2) + ' (TWD/kg)';
-    document.getElementById('output3').innerText = '一台斤價格（台幣）：' + output3.toFixed(2) + ' (TWD/台斤)';
+    document.getElementById('output1').innerText = '一公斤價格：' + output1.toFixed(2) + ' (USD/kg)';
+    document.getElementById('output2').innerText = '一公斤價格：' + output2.toFixed(2) + ' (TWD/kg)';
+    document.getElementById('output3').innerText = '一台斤價格：' + output3.toFixed(2) + ' (TWD/台斤)';
 });
